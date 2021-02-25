225e0226c3a7ba5821ff080fd17ee57c82a44064
rnjsalsrb7 <rnjsalsrb7@gmail.com>
2021-02-25 18:17:47 +0900

    homecontroller

--- TravelMaker/src/main/java/com/traveler/web/HomeController.java
+++ TravelMaker/src/main/java/com/traveler/web/HomeController.java
@@ -16,6 +16,11 @@ public class HomeController {
 	
 	@RequestMapping(value = "/")
 	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/main")
+	public String main() {
 		return "main";
 	}
 	

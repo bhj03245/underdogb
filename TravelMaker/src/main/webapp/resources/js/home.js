e9b55bf52392a11efc8b4b18e217838f9ce48b3d
kksskk1234 <kksskk1234@naver.com>
2021-03-01 17:09:01 +0900

    home

--- TravelMaker/src/main/webapp/resources/js/home.js
+++ TravelMaker/src/main/webapp/resources/js/home.js
@@ -1,6 +1,10 @@
 var signIn = document.getElementById('sign_in');
+var signUp = document.getElementById('sign_up');
 var loginModal = document.getElementById('login_modal');
+var signUpModal = document.getElementById('signUp_modal');
 var closeBtn = document.getElementById('close_btn');
+var closeBtnn = document.getElementById('close_btnn');
+
 
 signIn.onclick = function () {
 loginModal.style.display = 'inherit';
@@ -9,3 +13,12 @@ loginModal.style.display = 'inherit';
 closeBtn.onclick = function () {
 loginModal.style.display = 'none';
 };
+
+signUp.onclick = function () {
+signUpModal.style.display = 'inherit';
+};
+
+closeBtnn.onclick = function () {
+signUpModal.style.display = 'none';
+};
+

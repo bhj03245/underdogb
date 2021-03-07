15e6ab2fc63e8ce76ce5e52ab807c8d640a06f04
unknown <pnj0213@naver.com>
2021-03-07 18:14:27 +0900

    diary.sql

--- travelMakerSQL/travel_diary.sql
+++ travelMakerSQL/travel_diary.sql
@@ -1,16 +1,12 @@
-  
-CREATE TABLE traveler_diary(
-diary_no INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(100) NOT NULL,
-author VARCHAR(20) NOT NULL,
-diary VARCHAR(3000) NOT NULL,
-regdate DATE
-)
+CREATE TABLE traveler_diary( 
+diary_no INT AUTO_INCREMENT PRIMARY KEY, 
+title VARCHAR(100) NOT NULL, 
+diary VARCHAR(3000) NOT NULL, 
+writer VARCHAR(20) NOT NULL, 
+regdate DATE ,
+marker VARCHAR(3000)
+);
 
-SELECT * FROM traveler_diary;
-
-INSERT INTO traveler_diary(title, author, diary, regdate) VALUES ('정선 3일차', '아이디', '여기도내용', '2021-03-04')
+SELECT * FROM traveler_diary
 
 DROP TABLE traveler_diary
-
-SELECT * FROM traveler_diary ORDER BY diary_no DESC

6fa4cf8c46835eb22e6a029304ecf2903c5063fc
bhj03245 <bhj03245@naver.com>
2021-03-07 23:18:39 +0900

    journal_update_03_07_23:18

--- TravelMaker/src/main/java/com/traveler/web/journal/controller/JournalController.java
+++ TravelMaker/src/main/java/com/traveler/web/journal/controller/JournalController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.traveler.web.journal.model.JournalVO;
@@ -26,13 +27,31 @@ public class JournalController {
 	}
 	
 	@GetMapping("/create")
-	public String createJournal() throws Exception{
+	public String createJournal(@ModelAttribute("journalVO") JournalVO journalVO, Model model) throws Exception{
 		return "journal/createJournal";
 	}
 	
+	@GetMapping("/update")
+	public String updateJournal(@RequestParam("journal_no") int journal_no, @RequestParam("mode") String mode, Model model) throws Exception {
+		model.addAttribute("journalContent", journalService.getJournalContent(journal_no));
+		model.addAttribute("mode", mode);
+		model.addAttribute("journalVO", new JournalVO());
+		return "journal/updateJournal";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteBoard(RedirectAttributes rttr, @RequestParam("journal_no") int journal_no) throws Exception {
+		journalService.deleteJournal(journal_no);
+		return "redirect:/journal/list";
+	}
+	
 	@PostMapping("/saveJournal")
-	public String saveJouarnal(@ModelAttribute("JournalVO") JournalVO journalVO, RedirectAttributes rttr) throws Exception {
-		journalService.insertJournal(journalVO);
+	public String saveJouarnal(@ModelAttribute("JournalVO") JournalVO journalVO, @RequestParam("mode") String mode, RedirectAttributes rttr) throws Exception {
+		if (mode.equals("edit")) {
+			journalService.updateJournal(journalVO);
+		} else {
+			journalService.insertJournal(journalVO);
+		}
 		return "redirect:/journal/list";
 	}
 }
